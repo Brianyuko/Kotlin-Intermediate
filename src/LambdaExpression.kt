dceacce7acbc4a155d0e14afebe0347d0e5151ec
Brianyuko <brianyuko45@gmail.com>
2020-08-07 18:17:35 +0700

    Anonymous Function

--- src/LambdaExpression.kt
+++ src/LambdaExpression.kt
@@ -11,8 +11,24 @@ fun main(){
     // if one parameter, use it
     val lessThan : (Int) -> Boolean = { it < 20}
 
+    // Anonymous Function
+    val generateToken = fun(x: String) : String = "Token: $x"
+
     println(square(30))
     println(squareOther(16))
     println(lessThan(23))
     println(lessThan(19))
+    println(generateToken("#nabungGan"))
+
+    // Lambda Expression, passing parameter
+    val generateToken2 : (String) -> String = { "Token: $it" }
+    fun checkToken(userInput: String, createToken: (String) -> String){
+        val secretKey = "Token: 2020"
+        val userKey = createToken(userInput)
+
+        if (secretKey == userKey) print("Success")
+        else print("Failed")
+    }
+    // Passing parameter generateToken2
+    checkToken("2022", generateToken2)
 }
